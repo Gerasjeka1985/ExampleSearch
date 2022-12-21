3786f3de4e48c725c9befa298c7a07b0dda900cf
Gerasjeka1985 <gekgrom@mail.ru>
2022-12-22 00:34:59 +0300

    3 commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="layout-default">
+    <h4>
+      This is default layout
+    </h4>
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .layout-default h4{
+    color:green;
+  }
+</style>
+
